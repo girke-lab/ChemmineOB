29c8610ac8ddc6a2597823b8b061d2be440af853
khoran <khoran@cs.ucr.edu>
2013-07-16 13:25:54 -0700

    incorperate bug fix for loading openbabel plugins. see debugging branch

--- R/wrappers.R
+++ R/wrappers.R
@@ -32,6 +32,7 @@ convertFormat <- function(from,to,source){
 
 convertFormatFile <- function(from,to,fromFile,toFile){
 	.Call("ob_convert_file",as.character(from),as.character(to),as.character(fromFile),as.character(toFile),PACKAGE=packageName)
+<<<<<<< HEAD
 }
 
 prop_OB<- function(from,source) {
@@ -41,5 +42,8 @@ prop_OB<- function(from,source) {
 	df = as.data.frame(values)
 	colnames(df) = descriptorNames
 	df
+=======
+>>>>>>> incorperate bug fix for loading openbabel plugins. see debugging branch
 }
+
 
