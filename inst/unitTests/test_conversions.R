dc74aabcc3ede5ca7743844786b5ceff200bdbdc
k.horan <k.horan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-12-09 19:55:42 +0000

    fixed file based conversion, added test
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChemmineOB@84070 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_conversions.R
+++ inst/unitTests/test_conversions.R
@@ -16,4 +16,13 @@ test.formatConversions <- function(){
 	checkEquals(smiles,s2)
 
 	checkException(convertFormat("BADFORMAT","SDF",smiles))
+
+	t1=file.path(tempdir(),"t1.smi")
+	# 650001
+	cat("CC1=CC(=NO1)NC(=O)CCC(=O)N(CC(=O)NC2CCCC2)C3=CC4=C(C=C3)OCCO4",
+		 file=t1)
+	convertFormatFile("SMI","svg",t1,"test.svg")
+	checkTrue(file.exists("test.svg"))
+	checkTrue(file.info("test.svg")$size > 0)
+	unlink("test.svg")
 }
